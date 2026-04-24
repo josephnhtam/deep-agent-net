6b99c069d148e2c120e673b1bb5b6fe8a4d1d852
Joseph Tam <josephnhtam@gmail.com>
2026-04-25 00:27:30 +0800

    refactor: AzureOpenAIChatClientProvider env vars

--- samples/SampleUtilities/ChatClients/AzureOpenAIChatClientProvider.cs
+++ samples/SampleUtilities/ChatClients/AzureOpenAIChatClientProvider.cs
@@ -8,11 +8,11 @@ namespace SampleUtilities.ChatClients
     {
         public IChatClient GetChatClient()
         {
-            var apiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY")
-                ?? throw new InvalidOperationException("Set OPENAI_API_KEY environment variable.");
-            var endpoint = Environment.GetEnvironmentVariable("OPENAI_ENDPOINT")
-                ?? throw new InvalidOperationException("Set OPENAI_ENDPOINT environment variable.");
-            var modelName = Environment.GetEnvironmentVariable("OPENAI_DEPLOYMENT_NAME") ?? "gpt-5-mini";
+            var apiKey = Environment.GetEnvironmentVariable("AZURE_OPENAI_API_KEY")
+                ?? throw new InvalidOperationException("Set AZURE_OPENAI_API_KEY environment variable.");
+            var endpoint = Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT")
+                ?? throw new InvalidOperationException("Set AZURE_OPENAI_ENDPOINT environment variable.");
+            var modelName = Environment.GetEnvironmentVariable("AZURE_OPENAI_DEPLOYMENT_NAME") ?? "gpt-5-mini";
 
             return new AzureOpenAIClient(new Uri(endpoint), new ApiKeyCredential(apiKey))
                 .GetChatClient(modelName)
